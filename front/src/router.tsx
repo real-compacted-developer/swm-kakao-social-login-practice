955dcacace3a0db19d1c85bc6d20d82c43f74398
kkoon9 <rndrnjs2003@gmail.com>
2020-09-07 19:29:34 +0900

    init

---
+++ front/src/router.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+class Router extends React.Component {
+	render() {
+		return (
+			<BrowserRouter>
+				<Switch></Switch>
+			</BrowserRouter>
+		);
+	}
+}
+
+export default Router;
