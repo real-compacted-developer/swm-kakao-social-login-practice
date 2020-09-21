e6484c86c5b7c2ddb25fc80e56feb3c0d947467c
kkoon9 <rndrnjs2003@gmail.com>
2020-09-21 16:50:19 +0900

    fix : modify jwt module payload
    - payload에 isPremium 추가 및 email, nickname, role 삭제
    - try catch에서 catch 부분 err를 middleware에 던지는 걸로 코드 변경

--- back/module/jwt.js
+++ back/module/jwt.js
@@ -1,15 +1,11 @@
 const jwt = require('jsonwebtoken');
 const { secretOrPrivateKey } = require('../config/jwtSecretKey');
-const TOKEN_EXPIRED = -3;
-const TOKEN_INVALID = -2;
 
 module.exports = {
   sign: (user) => {
     const payload = {
       id: user.id,
-      email: user.email,
-      nickname: user.nickname,
-      role: user.role
+      isPremium: user.isPremium,
     };
     const options = {
       algorithm: "HS256", //H256(algorithm)에 해당하는 header, payload, signiture가 있음(내정)
@@ -32,18 +28,8 @@ module.exports = {
       // console.log(decodedPayload)
       return decodedPayload;
     } catch (err) {
-      if (err.message === 'jwt expired') {
-        console.log('expired token');
-        return TOKEN_EXPIRED;
-      } else if (err.message === 'invalid token') {
-        console.log('invalid token');
-        console.log(TOKEN_INVALID);
-        return TOKEN_INVALID;
-      } else {
-        console.log("invalid token");
-        return TOKEN_INVALID;
-      }
+      // console.log(err);
+      throw err;
     }
-    return decoded;
   },
 };
